e3462e91ecb76cc103ea1dcc1e0e9cc419c8e847
Magdalena Dmitruk <dmitrukm@gmail.com>
2024-08-14 16:21:09 +0000

    added tests for profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='adam', password='pass')
+
+    def test_can_list_profile(self):
+        adam = User.objects.get(username='adam')
+        response = self.client.get('/profiles/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+
+class ProfileDetailViewTests(APITestCase):
+    def setUp(self):
+        adam = User.objects.create_user(username='adam', password='pass')
+        colm = User.objects.create_user(username='colm', password='pass')
+
+    def test_can_retrieve_profile_using_valid_id(self):
+        response = self.client.get('/profiles/1/')
+        self.assertEqual(response.data['owner'], 'adam')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_profile_using_invalid_id(self):
+        response = self.client.get('/profiles/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_profile(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/profiles/1/', {'name': 'colm'})
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(profile.name, 'colm')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_profile(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put(
+            '/profiles/2/',
+            {'owner': 'colm'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
